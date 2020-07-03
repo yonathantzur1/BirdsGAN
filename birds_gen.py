68bdb986b09c2b8221011303c7d616921b162290
yonathantzur1 <yonathantzur1@gmail.com>
2020-07-03 18:14:14 +0300

    change gen script and update current output

--- birds_gen.py
+++ birds_gen.py
@@ -1,46 +1,49 @@
 import os
+import time
 
 import numpy as np
 from PIL import Image
 from tensorflow.keras.models import load_model
 
 NOISE_SIZE = 256
-PREVIEW_MARGIN = 1
 IMAGE_SIZE = 128
 
-PREVIEW_ROWS = 2
-PREVIEW_COLS = 2
-THRESHOLD = 0.9
-
+GENERATE_PATH = "gan_images"
+THRESHOLD = 0.98
+AMOUNT = 5
 
 GENERATOR_MODEL = "models/generator.h5"
 DISCRIMINATOR_MODEL = "models/discriminator.h5"
 
 
+def get_time():
+    return int(round(time.time() * 1000))
+
+
 def generate_image(generator, discriminator):
-    image_array = np.full((
-        PREVIEW_MARGIN + (PREVIEW_ROWS * (IMAGE_SIZE + PREVIEW_MARGIN)),
-        PREVIEW_MARGIN + (PREVIEW_COLS * (IMAGE_SIZE + PREVIEW_MARGIN)), 3), 255, dtype=np.uint8)
-
-    noise = np.random.normal(0, 1, (PREVIEW_ROWS * PREVIEW_COLS, NOISE_SIZE))
+    noise = np.random.normal(0, 1, (AMOUNT, NOISE_SIZE))
     generated_images = generator.predict(noise)
+    predict = discriminator.predict(generated_images)
 
-    while min(discriminator.predict(generated_images))[0] < THRESHOLD:
-        noise = np.random.normal(0, 1, (PREVIEW_ROWS * PREVIEW_COLS, NOISE_SIZE))
-        generated_images = generator.predict(noise)
+    while min(predict) < THRESHOLD:
+        false_indexes = []
+        for i in range(len(predict)):
+            if predict[i] < THRESHOLD:
+                false_indexes.append(i)
+        noise = np.random.normal(0, 1, (len(false_indexes), NOISE_SIZE))
+        fixed_images = generator.predict(noise)
+        for i in range(len(false_indexes)):
+            generated_images[false_indexes[i]] = fixed_images[i]
+        predict = discriminator.predict(generated_images)
 
-    print("acc: " + str(min(discriminator.predict(generated_images))[0]))
+    print("Minimum accuracy: " + str(min(predict)[0]))
     generated_images = 0.5 * generated_images + 0.5
-    image_count = 0
-    for row in range(PREVIEW_ROWS):
-        for col in range(PREVIEW_COLS):
-            r = row * (IMAGE_SIZE + PREVIEW_MARGIN) + PREVIEW_MARGIN
-            c = col * (IMAGE_SIZE + PREVIEW_MARGIN) + PREVIEW_MARGIN
-            image_array[r:r + IMAGE_SIZE, c:c + IMAGE_SIZE] = generated_images[image_count] * 255
-            image_count += 1
-    filename = os.path.join("./", "bird.png")
-    im = Image.fromarray(image_array)
-    im.save(filename)
+    for index in range(AMOUNT):
+        image_array = np.full((IMAGE_SIZE, IMAGE_SIZE, 3), 255, dtype=np.uint8)
+        image_array[0:0 + IMAGE_SIZE, 0:0 + IMAGE_SIZE] = generated_images[index] * 255
+        filename = os.path.join(GENERATE_PATH, str(get_time()) + ".png")
+        im = Image.fromarray(image_array)
+        im.save(filename)
 
 
 generator = load_model(GENERATOR_MODEL)
